517c8387dad438afc95a93a07f04542ff67c3319
Balogun Murtadho <murtadhob@gmail.com>
2022-12-28 01:19:39 +0700

    Task Completed

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,20 @@
-
-/* eslint-disable */
 export default class Building {
-  constructor(sqft = 0) {
-    if (
-      this.constructor !== Building && typeof
-      this.evacuationWarningMessage !== 'function'
-    ) {
-      throw new Error(
-        "Class extending Building must override evacuationWarningMessage"
-      );
-    }
-    this.sqft = sqft;
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error();
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
